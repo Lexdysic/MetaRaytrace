0a5ff0b6dd957d04bb6739974b91a21efaac9a37
Lexdysic <lexdysic@gmail.com>
2018-02-14 22:33:44 -0800

    Remove dependency on std::string

--- Code/Main.cpp
+++ Code/Main.cpp
@@ -1,23 +1,22 @@
 #include "Meta.h"
-#include <string>
+#include <string.h>
 
 //===========================================================================
-void find_replace(std::string & str, const std::string & s, const std::string & r) {
-    std::string::size_type pos = 0;
-    while ((pos = str.find(s, pos)) != std::string::npos) {
-        str.replace(pos, s.size(), r);
-        pos++;
+void find_replace (char * str, char what, char with) {
+    for (char * it = str; *it != '\0'; ++it) {
+        if (*it == what)
+            *it = with;
     }
 }
 
 //=====================================s======================================
 int main () {
-    typedef Renderer<200, 200> MyRenderer;
+    typedef Renderer<100, 100> MyRenderer;
 
+    char filename[64];
+    strcpy(filename, "Image - " __DATE__ " - " __TIME__ ".tga");
+    find_replace(filename, ':', '_');
 
-    std::string filename = "Image - " __DATE__ " - " __TIME__ ".tga";
-    find_replace(filename, ":", "_");
-
-    MyRenderer::Save(filename.c_str());
+    MyRenderer::Save(filename);
 
 }
